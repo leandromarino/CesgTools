ab62b7bb401f389bfc44bfab5b11c46104efecb1
Leandro Marino <leandromarino@leandromarino.com.br>
2022-01-03 10:27:13 -0300

    bug na exportacao com gabpar

--- R/write.ctt.R
+++ R/write.ctt.R
@@ -22,7 +22,7 @@ write.ctt <- function(ctt, gabpar = NULL, colsgabpar = NULL, exporta_dir){
   for(i in c(1,3)) ctt[[i]] <- ctt[[i]][,!is.element(colnames(ctt[[i]]),cols_remove)]
   
   # juntando com informacoes de gabpar
-  if( is.null(gabpar)){ctt[[3]] <- cbind(gabpar[,colsgabpar],ctt[[1]])}
+  if(!is.null(gabpar)){ctt[[1]] <- cbind(gabpar[,colsgabpar],ctt[[1]])}
   if(!is.null(gabpar)){ctt[[3]] <- cbind(gabpar[as.integer(ctt[[3]]$IT),colsgabpar],ctt[[3]])}
   
   xlsx::write.xlsx(ctt[[1]], paste0(exporta_dir,nmctt,'.xlsx'), sheetName = 'Estatisticas'  , showNA = FALSE, row.names = FALSE)

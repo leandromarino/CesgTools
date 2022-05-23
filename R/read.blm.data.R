89e775a8ce206e696d384c686c3230e7034f6242
Leandro Marino <leandromarino@leandromarino.com.br>
2022-05-23 10:20:18 -0300

    deprecated info added

--- R/read.blm.data.R
+++ R/read.blm.data.R
@@ -1,10 +1,9 @@
 
 
 read.blm.data <- function(file, stru){
+  warning('Função ficando fora de uso. Substituir pela read.blm_txt')
   # stru = estrutura (da funcao read.blm.stru)
   #colocar um controle de classes
   data <- read.fwf(file = file, widths = stru$stru, header = FALSE, colClasses = 'character', col.names = rownames(stru))
   data
 }
-
-
